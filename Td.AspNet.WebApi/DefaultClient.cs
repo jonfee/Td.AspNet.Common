87ee6221da83f425f86c6136163e560041574c62
明月 <hibruin@outlook.com>
2015-11-23 16:29:19 +0800

    添加文件post的支持

--- Td.AspNet.WebApi/DefaultClient.cs
+++ Td.AspNet.WebApi/DefaultClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Td.AspNet.Utils;
 
@@ -71,6 +72,70 @@ namespace Td.AspNet.WebApi
             url = Strings.BuildGetUrl(url, urlParams);
 
             HttpContent content = new FormUrlEncodedContent(parameters);
+
+            HttpClient client = new HttpClient();
+            //指定返回XML格式
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
+            //指定返回JSON
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/json")); 
+            var txt = string.Empty;
+            using (client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.PostAsync(url, content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        txt = await response.Content.ReadAsStringAsync();
+                    }
+                }
+                catch (Exception ex) { }
+            }
+            return txt;
+        }
+
+        /// <summary>
+        /// 通过Post方式调用WebApi,包含文件上传
+        /// </summary>
+        /// <param name="url">api url</param>
+        /// <param name="parameters">表单数据字典</param>
+        /// <param name="files">表单中要上传的文件</param>
+        /// <param name="partnerid">合作者或模块ID</param>
+        /// <param name="secretKey">加密参数</param>
+        /// <returns></returns>
+        public static async Task<string> DoPost(string url, IDictionary<string, string> parameters, List<FileContent> files, string partnerid, string secretKey)
+        {
+
+            IDictionary<string, string> txtParams = new Dictionary<string, string>(parameters);
+            IDictionary<string, string> urlParams = new Dictionary<string, string>();
+            urlParams.Add("PartnerId", partnerid);
+            urlParams.Add("Timestamp", DateTime.Now.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+
+            foreach (var item in urlParams)
+            {
+                txtParams.Add(item.Key, item.Value);
+            }
+
+            var sign = Strings.SignRequest(txtParams, secretKey);
+            urlParams.Add("Sign", sign);
+
+            url = Strings.BuildGetUrl(url, urlParams);
+
+            var content = new MultipartFormDataContent();
+            foreach (var item in parameters)
+            {
+                content.Add(new StringContent(item.Value), item.Key);
+            }
+
+            foreach (var file in files)
+            {
+                StreamContent fileContent = new StreamContent(file.FileSteam);
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data");
+                fileContent.Headers.ContentDisposition.FileName = file.FileName;
+
+                content.Add(fileContent);
+            }
 
             HttpClient client = new HttpClient();
             //指定返回XML格式
---
+++ Td.AspNet.WebApi/FileContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Td.AspNet.WebApi
+{
+    public class FileContent
+    {
+        public string FileName { get; set; }
+
+        public Stream FileSteam { get; set; }
+    }
+}
