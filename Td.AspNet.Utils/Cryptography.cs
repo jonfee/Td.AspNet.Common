a3545b0ddafcdc52795743bc9c0d3d485b6c02e2
袁坷 <562573053@qq.com>
2016-06-06 19:47:40 +0800

    MD5加密

--- Td.AspNet.Utils/Cryptography.cs
+++ Td.AspNet.Utils/Cryptography.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -34,5 +35,149 @@ namespace Td.AspNet.Utils
             }
             return EnText.ToString();
         }
+        /// <summary>
+        /// Hash 加密采用的算法
+        /// </summary>
+        public enum HashFormat
+        {
+            MD516,
+            MD532,
+            SHA1,
+            SHA256,
+            SHA384,
+            SHA512
+        };
+
+        /// <summary>
+        /// 对称加密采用的算法
+        /// </summary>
+        public enum SymmetricFormat
+        {
+            DES,
+            TripleDES
+        };
+
+        /// <summary>
+        /// 对字符串进行 Hash 加密
+        /// </summary>
+        public static string Hash(string inputString, HashFormat hashFormat)
+        {
+            HashAlgorithm algorithm = null;
+
+            switch (hashFormat)
+            {
+                case HashFormat.MD516:
+                    algorithm = MD5.Create();
+                    break;
+                case HashFormat.MD532:
+                    algorithm = MD5.Create();
+                    break;
+                case HashFormat.SHA1:
+                    algorithm = SHA1.Create();
+                    break;
+                case HashFormat.SHA256:
+                    algorithm = SHA256.Create();
+                    break;
+                case HashFormat.SHA384:
+                    algorithm = SHA384.Create();
+                    break;
+                case HashFormat.SHA512:
+                    algorithm = SHA512.Create();
+                    break;
+            }
+
+            algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+
+            if (hashFormat == HashFormat.MD516)
+            {
+                return BitConverter.ToString(algorithm.Hash).Replace("-", "").Substring(8, 16).ToUpper();
+            }
+            else
+            {
+                return BitConverter.ToString(algorithm.Hash).Replace("-", "").ToUpper();
+            }
+        }
+
+        /// <summary>
+        /// 对字符串进行对称加密
+        /// </summary>
+        public static string SymmetricEncrypt(string inputString, SymmetricFormat symmetricFormat, string key, string iv)
+        {
+            SymmetricAlgorithm algorithm = null;
+
+            switch (symmetricFormat)
+            {
+                case SymmetricFormat.DES:
+                    algorithm = DES.Create();
+                    break;
+                case SymmetricFormat.TripleDES:
+                    algorithm = TripleDES.Create();
+                    break;
+            }
+
+            int keySize = algorithm.Key.Length;
+
+            byte[] desString = Encoding.UTF8.GetBytes(inputString);
+
+            byte[] desKey = Encoding.UTF8.GetBytes(key.Substring(0, keySize));
+
+            byte[] desIV = Encoding.UTF8.GetBytes(iv.Substring(0, keySize));
+
+            MemoryStream mStream = new MemoryStream();
+
+            CryptoStream cStream = new CryptoStream(mStream, algorithm.CreateEncryptor(desKey, desIV), CryptoStreamMode.Write);
+
+            cStream.Write(desString, 0, desString.Length);
+
+            cStream.FlushFinalBlock();
+
+            cStream.Close();
+
+            return BitConverter.ToString(mStream.ToArray()).Replace("-", "").ToUpper();
+        }
+
+        /// <summary>
+        /// 对字符串进行对称解密
+        /// </summary>
+        public static string SymmetricDecrypt(string inputString, SymmetricFormat symmetricFormat, string key, string iv)
+        {
+            SymmetricAlgorithm algorithm = null;
+
+            switch (symmetricFormat)
+            {
+                case SymmetricFormat.DES:
+                    algorithm = DES.Create();
+                    break;
+                case SymmetricFormat.TripleDES:
+                    algorithm = TripleDES.Create();
+                    break;
+            }
+
+            int keySize = algorithm.Key.Length;
+
+            byte[] desString = new byte[inputString.Length / 2];
+
+            for (int i = 0; i < inputString.Length; i += 2)
+            {
+                desString[i / 2] = byte.Parse(inputString.Substring(i, 2), System.Globalization.NumberStyles.AllowHexSpecifier);
+            }
+
+            byte[] desKey = Encoding.UTF8.GetBytes(key.Substring(0, keySize));
+
+            byte[] desIV = Encoding.UTF8.GetBytes(iv.Substring(0, keySize));
+
+            MemoryStream mStream = new MemoryStream();
+
+            CryptoStream cStream = new CryptoStream(mStream, algorithm.CreateDecryptor(desKey, desIV), CryptoStreamMode.Write);
+
+            cStream.Write(desString, 0, desString.Length);
+
+            cStream.FlushFinalBlock();
+
+            cStream.Close();
+
+            return Encoding.UTF8.GetString(mStream.ToArray());
+        }
+
     }
 }
