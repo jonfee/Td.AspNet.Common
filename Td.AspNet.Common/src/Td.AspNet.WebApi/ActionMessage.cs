b84214de587ab9db1b2c2a2adcf2620135b88ed7
yk <yk@DESKTOP-97CR00P>
2016-07-14 15:27:22 +0800

    9.0.1

--- Td.AspNet.Common/src/Td.AspNet.WebApi/ActionMessage.cs
+++ Td.AspNet.Common/src/Td.AspNet.WebApi/ActionMessage.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
-using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Td.AspNet.WebApi
 {
     public class ActionMessage
     {
-        public static IActionResult Ok(ErrorMessage message)
-        {
-            var result = new HttpOkObjectResult("api");
-            result.Value = Newtonsoft.Json.JsonConvert.SerializeObject(message);
-            result.StatusCode = 200;
-            result.ContentTypes.Add(new MediaTypeHeaderValue("application/json"));
-            return result;
-        }
+        //public static IActionResult Ok(ErrorMessage message)
+        //{
+        //    var result = new HttpOkObjectResult("api");
+        //    result.Value = Newtonsoft.Json.JsonConvert.SerializeObject(message);
+        //    result.StatusCode = 200;
+        //    result.ContentTypes.Add(new Microsoft.Net.Http.Headers.MediaTypeHeaderValue("application/json"));
+        //    return result;
+        //}
     }
 }
