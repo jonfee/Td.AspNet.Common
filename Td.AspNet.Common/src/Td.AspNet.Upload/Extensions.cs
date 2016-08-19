46f372a7b5021c6c91d4f95ffaed34ba2fe15adb
伍朝辉 <337883612@qq.com>
2016-08-19 14:08:46 +0800

    新增对Base64图片文件的上传支持

--- Td.AspNet.Common/src/Td.AspNet.Upload/Extensions.cs
+++ Td.AspNet.Common/src/Td.AspNet.Upload/Extensions.cs
@@ -15,6 +15,97 @@ namespace Td.AspNet.Upload
     public static class Extensions
     {
         /// <summary>
+        /// 添加FormFile文件流
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="file"></param>
+        /// <param name="limitSize"></param>
+        /// <param name="filter"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        private static MultipartFormDataContent AddFormFileContent(this MultipartFormDataContent content, FormFileContent file, long limitSize, string[] filter, out string error)
+        {
+            error = string.Empty;
+
+            #region //文件对象不存在时，忽略
+            if (null == file || file.FormFile == null || file.FileSize < 1)
+            {
+                error = "文件对象不存在";
+            }
+            #endregion
+            #region//文件大小限制检测
+            else if (limitSize > 0 && file.FileSize > limitSize * 1024)
+            {
+                error = string.Format("上传文件最大不能超过{0}KB", limitSize);
+            }
+            #endregion
+            #region//文件格式限制检测
+            else if (filter != null && filter.Length > 0)
+            {
+                //当前文件扩展名
+                string currentExt = Path.GetExtension(file.FormFile.FileName).ToLower();
+
+                //非有效文件格式时，返回提示
+                if (!filter.Contains(currentExt))
+                {
+                    error = string.Format("上传文件类型失败，限制为{0}", string.Join("|", filter));
+                }
+            }
+            #endregion
+
+            #region//文件正常，则添加文件对象到Post数据
+            if (string.IsNullOrWhiteSpace(error))
+            {
+                StreamContent fileContent = new StreamContent(file.FormFile.OpenReadStream());//文件流
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);//文件内容类型
+                fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data");//form-data表示一个表单对象数据
+                fileContent.Headers.ContentDisposition.FileName = file.FormFile.FileName;//文件名
+                fileContent.Headers.ContentDisposition.Name = file.FieldName;//文件标识字段名称
+
+                content.Add(fileContent);
+            }
+            #endregion
+
+            return content;
+        }
+
+        /// <summary>
+        /// 添加Base64文件流
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="file"></param>
+        /// <param name="filedName">标志字段名</param>
+        /// <param name="limitSize"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        private static MultipartFormDataContent AddBase64FileContent(this MultipartFormDataContent content, Base64FileContent file, long limitSize, out string error)
+        {
+            error = string.Empty;
+
+            #region //文件对象不存在时，忽略
+            if (null == file || file.Data == null || file.FileSize < 1)
+            {
+                error = "文件对象不存在";
+            }
+            #endregion
+            #region//文件大小限制检测
+            else if (limitSize > 0 && file.FileSize > limitSize * 1024)
+            {
+                error = string.Format("上传文件最大不能超过{0}KB", limitSize);
+            }
+            #endregion
+
+            #region//文件正常，则添加文件对象到Post数据
+            if (string.IsNullOrWhiteSpace(error))
+            {
+                content.Add(new StringContent(file.Data), file.FieldName);
+            }
+            #endregion
+
+            return content;
+        }
+
+        /// <summary>
         /// 保存文件
         /// </summary>
         /// <param name="context">上传文件上下文</param>
@@ -26,7 +117,7 @@ namespace Td.AspNet.Upload
             List<UploadResult> result = new List<UploadResult>();
 
             //文件上传对象不存在时，返回null
-            if (null == context || context.FormFileList.Count < 1) return null;
+            if (null == context || context.FileList.Count < 1) return null;
 
             //处理文件上传
             using (var postContent = new MultipartFormDataContent())
@@ -35,7 +126,7 @@ namespace Td.AspNet.Upload
                 int effectCount = 0;
 
                 #region 1、//遍历上传文件对象
-                foreach (var fc in context.FormFileList)
+                foreach (var fc in context.FileList)
                 {
                     #region//预定义上传结果
                     var itemRst = new UploadResult
@@ -43,52 +134,34 @@ namespace Td.AspNet.Upload
                         FieldName = fc.FieldName,
                         FileName = context.UploadName,
                         FilePath = "",
-                        FileSize = fc.FormFile.Length,
+                        FileSize = fc.FileSize,
                         ContentType = fc.ContentType,
                         IsImage = fc.IsImage,
                         Message = ""
                     };
                     #endregion
-                    #region //文件对象不存在时，忽略
-                    if (null == fc || fc.FormFile == null || fc.FormFile.Length < 1)
+
+                    string error = null;
+
+                    //FormFile 对象流
+                    if (fc is FormFileContent)
                     {
-                        itemRst.Message = "文件对象不存在";
+                        var file = (FormFileContent)fc;
+
+                        postContent.AddFormFileContent(file, context.MaxSize, context.Filter, out error);
                     }
-                    #endregion
-                    #region//文件大小限制检测
-                    else if (context.MaxSize > 0 && fc.FormFile.Length > context.MaxSize * 1024)
+                    //Base64图片文件流
+                    else if (fc is Base64FileContent)
                     {
-                        itemRst.Message = string.Format("上传文件最大不能超过{0}KB", context.MaxSize);
+                        var base64 = (Base64FileContent)fc;
+
+                        postContent.AddBase64FileContent(base64, context.MaxSize, out error);
                     }
-                    #endregion
-                    #region//文件格式限制检测
-                    else if (context.Filter != null && context.Filter.Length > 0)
+
+                    if (string.IsNullOrWhiteSpace(error))
                     {
-                        //当前文件扩展名
-                        string currentExt = Path.GetExtension(fc.FormFile.FileName).ToLower();
-
-                        //非有效文件格式时，返回提示
-                        if (!context.Filter.Contains(currentExt))
-                        {
-                            itemRst.Message = string.Format("上传文件类型失败，限制为{0}", string.Join("|", context.Filter));
-                        }
-                    }
-                    #endregion
-
-                    #region//文件正常，则添加文件对象到Post数据
-                    if (string.IsNullOrWhiteSpace(itemRst.Message))
-                    {
-                        StreamContent fileContent = new StreamContent(fc.FormFile.OpenReadStream());//文件流
-                        fileContent.Headers.ContentType = new MediaTypeHeaderValue(fc.ContentType);//文件内容类型
-                        fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data");//form-data表示一个表单对象数据
-                        fileContent.Headers.ContentDisposition.FileName = fc.FormFile.FileName;//文件名
-                        fileContent.Headers.ContentDisposition.Name = fc.FieldName;//文件标识字段名称
-
-                        postContent.Add(fileContent);
-
                         effectCount++;
                     }
-                    #endregion
 
                     result.Add(itemRst);
                 }
--- Td.AspNet.Common/src/Td.AspNet.Upload/UploadContext.cs
+++ Td.AspNet.Common/src/Td.AspNet.Upload/UploadContext.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Td.AspNet.Upload
 {
@@ -12,7 +13,7 @@ namespace Td.AspNet.Upload
     /// </summary>
     public class UploadContext
     {
-        #region 单个文件上传实例
+        #region 单个文件FormFile上传实例
 
         /// <summary>
         /// 实始化文件上传内容实例（自动随机生成文件名称）
@@ -74,7 +75,7 @@ namespace Td.AspNet.Upload
 
         #endregion
 
-        #region 多文件上传实例
+        #region 多文件FormFile上传实例
 
         /// <summary>
         /// 实始化文件上传内容实例（自动随机生成文件名称）
@@ -146,11 +147,11 @@ namespace Td.AspNet.Upload
             this.CutIfOut = cutIfOut;
 
             //文件
-            this.FormFileList = files.Where(file => file != null && file.Length > 0)
+            this.FileList = files.Where(file => file != null && file.Length > 0)
                 .Select(file => new FormFileContent
                 {
                     FormFile = file
-                }).ToList();
+                }).ToList<FileContent>();
 
             //格式限制
             if (!string.IsNullOrWhiteSpace(filter))
@@ -169,6 +170,235 @@ namespace Td.AspNet.Upload
         }
 
         #endregion
+
+        #region 单个文件Base64上传实例
+
+        /// <summary>
+        /// 实始化文件上传内容实例（自动随机生成文件名称）
+        /// </summary
+        /// <param name="apiAddress">上传API地址</param>
+        /// <param name="base64String">待上传的文件标志字段及Base64字符串</param>
+        /// <param name="maxSize">最大允许上传的文件大小（单位：KB）</param>
+        /// <param name="uploadFolder">需保存的文件夹路径（从根路径后）</param>
+        /// <param name="isFixedPath">是否固定存储在指定的<paramref name="uploadFolder"/>目录下，为false时在<paramref name="uploadFolder"/>目录后添加日期目录</param>
+        public UploadContext(string apiAddress, KeyValuePair<string, string> fieldAndBase64String, long maxSize, string uploadFolder, bool isFixedPath)
+            : this(apiAddress, fieldAndBase64String, maxSize, uploadFolder, isFixedPath, null) { }
+
+        /// <summary>
+        /// 实始化文件上传内容实例（存在同名文件时不覆盖）
+        /// </summary>
+        /// <param name="apiAddress">上传API地址</param>
+        /// <param name="base64String">待上传的文件标志字段及Base64字符串</param>
+        /// <param name="maxSize">最大允许上传的文件大小（单位：KB）</param>
+        /// <param name="uploadFolder">需保存的文件夹路径（从根路径后）</param>
+        /// <param name="isFixedPath">是否固定存储在指定的<paramref name="uploadFolder"/>目录下，为false时在<paramref name="uploadFolder"/>目录后添加日期目录</param>
+        /// <param name="uploadName">重命名后的文件名称（为null时随机生成）</param>
+        public UploadContext(string apiAddress, KeyValuePair<string, string> fieldAndBase64String, long maxSize, string uploadFolder, bool isFixedPath, string uploadName)
+            : this(apiAddress, fieldAndBase64String, maxSize, uploadFolder, isFixedPath, uploadName, null) { }
+
+        /// <summary>
+        /// 实始化文件上传内容实例
+        /// </summary>
+        /// <param name="apiAddress">上传API地址</param>
+        /// <param name="fieldAndBase64String">待上传的文件标志字段及Base64字符串</param>
+        /// <param name="maxSize">最大允许上传的文件大小（单位：KB）</param>
+        /// <param name="uploadFolder">需保存的文件夹路径（从根路径后）</param>
+        /// <param name="isFixedPath">是否固定存储在指定的<paramref name="uploadFolder"/>目录下，为false时在<paramref name="uploadFolder"/>目录后添加日期目录</param>
+        /// <param name="uploadName">重命名后的文件名称（为null时随机生成）</param>
+        /// <param name="extensionName">扩展名（如：jpg）</param>
+        public UploadContext(string apiAddress, KeyValuePair<string, string> fieldAndBase64String, long maxSize, string uploadFolder, bool isFixedPath, string uploadName, string extensionName)
+            : this(apiAddress, fieldAndBase64String, maxSize, uploadFolder, isFixedPath, uploadName, extensionName, false) { }
+
+        /// <summary>
+        /// 实始化文件上传内容实例
+        /// </summary>
+        /// <param name="apiAddress">上传API地址</param>
+        /// <param name="fieldAndBase64String">待上传的文件标志字段及Base64字符串</param>
+        /// <param name="maxSize">最大允许上传的文件大小（单位：KB）</param>
+        /// <param name="uploadFolder">需保存的文件夹路径（从根路径后）</param>
+        /// <param name="isFixedPath">是否固定存储在指定的<paramref name="uploadFolder"/>目录下，为false时在<paramref name="uploadFolder"/>目录后添加日期目录</param>
+        /// <param name="uploadName">重命名后的文件名称（为null时随机生成）</param>
+        /// <param name="extensionName">扩展名（如：jpg）</param>
+        /// <param name="beOverride">存在同名文件时是否覆盖</param>
+        /// <param name="compressIfGreaterSize">超过该大小时进行质量压缩（单位：KB）,该参数只对图片上传有效</param>
+        /// <param name="maxWidth">图片最大宽（超出时裁剪/压缩）</param>
+        /// <param name="maxHeight">图片最大高（超出时裁剪/压缩）</param>
+        /// <param name="cutIfOut">超出图片限制的宽/高时是否裁剪（为false时不足部分留白）</param>
+        public UploadContext(string apiAddress, KeyValuePair<string, string> fieldAndBase64String, long maxSize, string uploadFolder, bool isFixedPath, string uploadName = null, string extensionName = null, bool beOverride = false, long compressIfGreaterSize = 0, int maxWidth = 0, int maxHeight = 0, bool cutIfOut = false)
+            : this(apiAddress, new Dictionary<string, string>() { { fieldAndBase64String.Key, fieldAndBase64String.Value } }, maxSize, uploadFolder, isFixedPath, uploadName, extensionName, beOverride, compressIfGreaterSize, maxWidth, maxHeight, cutIfOut) { }
+
+        #endregion
+
+        #region 多文件Base64上传实例
+
+        /// <summary>
+        /// 实始化文件上传内容实例（自动随机生成文件名称）
+        /// </summary
+        /// <param name="apiAddress">上传API地址</param>
+        /// <param name="fieldAndBase64Strings">待上传的文件标志字段名称及Base64字符串集合</param>
+        /// <param name="maxSize">最大允许上传的文件大小（单位：KB）</param>
+        /// <param name="uploadFolder">需保存的文件夹路径（从根路径后）</param>
+        /// <param name="isFixedPath">是否固定存储在指定的<paramref name="uploadFolder"/>目录下，为false时在<paramref name="uploadFolder"/>目录后添加日期目录</param>
+        public UploadContext(string apiAddress, IDictionary<string, string> fieldAndBase64Strings, long maxSize, string uploadFolder, bool isFixedPath)
+                    : this(apiAddress, fieldAndBase64Strings, maxSize, uploadFolder, isFixedPath, null) { }
+
+        /// <summary>
+        /// 实始化文件上传内容实例（存在同名文件时不覆盖）
+        /// </summary>
+        /// <param name="apiAddress">上传API地址</param>
+        /// <param name="fieldAndBase64Strings">待上传的文件标志字段名称及Base64字符串集合</param>
+        /// <param name="maxSize">最大允许上传的文件大小（单位：KB）</param>
+        /// <param name="uploadFolder">需保存的文件夹路径（从根路径后）</param>
+        /// <param name="isFixedPath">是否固定存储在指定的<paramref name="uploadFolder"/>目录下，为false时在<paramref name="uploadFolder"/>目录后添加日期目录</param>
+        /// <param name="uploadName">重命名后的文件名称（为null时随机生成）</param>
+        public UploadContext(string apiAddress, IDictionary<string, string> fieldAndBase64Strings, long maxSize, string uploadFolder, bool isFixedPath, string uploadName)
+                    : this(apiAddress, fieldAndBase64Strings, maxSize, uploadFolder, isFixedPath, uploadName, null) { }
+
+        /// <summary>
+        /// 实始化文件上传内容实例
+        /// </summary>
+        /// <param name="apiAddress">上传API地址</param>
+        /// <param name="fieldAndBase64Strings">待上传的文件标志字段名称及Base64字符串集合</param>
+        /// <param name="maxSize">最大允许上传的文件大小（单位：KB）</param>
+        /// <param name="uploadFolder">需保存的文件夹路径（从根路径后）</param>
+        /// <param name="isFixedPath">是否固定存储在指定的<paramref name="uploadFolder"/>目录下，为false时在<paramref name="uploadFolder"/>目录后添加日期目录</param>
+        /// <param name="uploadName">重命名后的文件名称（为null时随机生成）</param>
+        /// <param name="extensionName">扩展名（如：jpg）</param>
+        public UploadContext(string apiAddress, IDictionary<string, string> fieldAndBase64Strings, long maxSize, string uploadFolder, bool isFixedPath, string uploadName, string extensionName)
+                    : this(apiAddress, fieldAndBase64Strings, maxSize, uploadFolder, isFixedPath, uploadName, extensionName, false, 0, 0, 0, false) { }
+
+        /// <summary>
+        /// 实始化文件上传内容实例
+        /// </summary>
+        /// <param name="apiAddress">上传API地址</param>
+        /// <param name="fieldAndBase64Strings">待上传的文件标志字段名称及Base64字符串集合</param>
+        /// <param name="maxSize">最大允许上传的文件大小（单位：KB）</param>
+        /// <param name="uploadFolder">需保存的文件夹路径（从根路径后）</param>
+        /// <param name="isFixedPath">是否固定存储在指定的<paramref name="uploadFolder"/>目录下，为false时在<paramref name="uploadFolder"/>目录后添加日期目录</param>
+        /// <param name="uploadName">重命名后的文件名称（为null时随机生成）</param>
+        /// <param name="extensionName">扩展名（如：jpg）</param>
+        /// <param name="beOverride">存在同名文件时是否覆盖</param>
+        /// <param name="compressIfGreaterSize">超过该大小时进行质量压缩（单位：KB）,该参数只对图片上传有效</param>
+        /// <param name="maxWidth">图片最大宽（超出时裁剪/压缩）</param>
+        /// <param name="maxHeight">图片最大高（超出时裁剪/压缩）</param>
+        /// <param name="cutIfOut">超出图片限制的宽/高时是否裁剪（为false时不足部分留白）</param>
+        public UploadContext(string apiAddress, IDictionary<string, string> fieldAndBase64Strings, long maxSize, string uploadFolder, bool isFixedPath, string uploadName = null, string extensionName = null, bool beOverride = false, long compressIfGreaterSize = 0, int maxWidth = 0, int maxHeight = 0, bool cutIfOut = false)
+        {
+            this.ApiAddress = apiAddress ?? string.Empty;
+            this.MaxSize = maxSize;
+            this.UploadFolder = uploadFolder ?? string.Empty;
+            this.IsFixedPath = isFixedPath;
+            this.UploadName = uploadName ?? string.Empty;
+            this.SaveExtension = extensionName ?? string.Empty;
+            this.BeOverride = beOverride;
+            this.CompressIfGreaterSize = compressIfGreaterSize;
+            this.MaxWidth = maxWidth;
+            this.MaxHeight = maxHeight;
+            this.CutIfOut = cutIfOut;
+
+            //文件
+            this.FileList = fieldAndBase64Strings.Select(p => new Base64FileContent
+            {
+                FieldName = p.Key,
+                Data = p.Value,
+            }).ToList<FileContent>();
+        }
+
+        #endregion
+
+        #region 方法
+
+        /// <summary>
+        /// 添加FormFile文件内容
+        /// </summary>
+        /// <param name="file"></param>
+        public void AddFormFile(IFormFile file)
+        {
+            if (file != null)
+            {
+                this.FileList.Add(new FormFileContent
+                {
+                    FormFile = file
+                });
+            }
+        }
+
+        /// <summary>
+        /// 添加FormFile文件内容
+        /// </summary>
+        /// <param name="files"></param>
+        public void AddFormFile(IEnumerable<IFormFile> files)
+        {
+            if (files != null && files.Count() > 0)
+            {
+               var newFiles= files.Where(file => file != null && file.Length > 0)
+                .Select(file => new FormFileContent
+                {
+                    FormFile = file
+                }).ToList<FileContent>();
+
+                this.FileList.AddRange(newFiles);
+            }
+        }
+
+        #region 添加Base64文件数据
+
+        /// <summary>
+        /// 添加Base64图片文件内容
+        /// </summary>
+        /// <param name="fieldName">标志名称</param>
+        /// <param name="base64String">Base64文件内容</param>
+        public void AddBase64File(string fieldName, string base64String)
+        {
+            if (!string.IsNullOrWhiteSpace(fieldName) && !string.IsNullOrWhiteSpace(base64String))
+            {
+                this.FileList.Add(new Base64FileContent
+                {
+                    FieldName = fieldName,
+                    Data = base64String,
+                });
+            }
+        }
+
+        /// <summary>
+        /// 添加Base64图片文件内容
+        /// </summary>
+        /// <param name="fieldAndBase64String">标志名称 及 Base64文件内容</param>
+        public void AddBase64File(KeyValuePair<string, string> fieldAndBase64String)
+        {
+            if (!fieldAndBase64String.Equals(default(KeyValuePair<string, string>)))
+            {
+                this.FileList.Add(new Base64FileContent
+                {
+                    FieldName = fieldAndBase64String.Key,
+                    Data = fieldAndBase64String.Value,
+                });
+            }
+        }
+
+        /// <summary>
+        /// 添加Base64图片文件内容
+        /// </summary>
+        /// <param name="fieldAndBase64Strings">标志名称 及 Base64文件内容 集合</param>
+        public void AddBase64File(IDictionary<string, string> fieldAndBase64Strings)
+        {
+            if (fieldAndBase64Strings != null && fieldAndBase64Strings.Count > 0)
+            {
+                var files = fieldAndBase64Strings.Select(p => new Base64FileContent
+                {
+                    FieldName = p.Key,
+                    Data = p.Value,
+                }).ToList<FileContent>();
+
+                this.FileList.AddRange(files);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region 属性
 
         /// <summary>
         /// 允许上传的文件格式
@@ -208,12 +438,12 @@ namespace Td.AspNet.Upload
         /// <summary>
         /// 文件对象集合
         /// </summary>
-        public List<FormFileContent> FormFileList { get; private set; }
+        public List<FileContent> FileList { get; private set; }
 
         /// <summary>
         /// 是否为批量上传
         /// </summary>
-        public bool IsBatchUpload { get { return FormFileList != null && FormFileList.Count > 0; } }
+        public bool IsBatchUpload { get { return FileList != null && FileList.Count > 0; } }
 
         /// <summary>
         /// 文件保存的文件夹路径
@@ -239,26 +469,78 @@ namespace Td.AspNet.Upload
         /// 上传文件名
         /// </summary>
         public string UploadName { get; private set; }
+
+        #endregion
+    }
+
+    #region 文件信息类
+
+    /// <summary>
+    /// 文件内容
+    /// </summary>
+    public abstract class FileContent
+    {
+        /// <summary>
+        /// 文件标志名
+        /// </summary>
+        public virtual string FieldName { get; set; }
+
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        public virtual string ContentType { get; }
+
+        /// <summary>
+        /// 是否为图片文件
+        /// </summary>
+        public bool IsImage
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(ContentType))
+                {
+                    return ContentType.StartsWith("image/", StringComparison.OrdinalIgnoreCase);
+                }
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public abstract long FileSize { get; }
     }
 
     /// <summary>
-    /// 上传文件内容
+    /// FormFileContent
     /// </summary>
-    public class FormFileContent
+    public class FormFileContent : FileContent
     {
         /// <summary>
         /// 文件
         /// </summary>
         public IFormFile FormFile { get; set; }
 
+        private string _fieldName;
         /// <summary>
         /// 文件标志名
         /// </summary>
-        public string FieldName
+        public override string FieldName
         {
             get
             {
-                return null != HeaderValue ? HeaderValue.Name : string.Empty;
+                if (string.IsNullOrWhiteSpace(_fieldName) && FormFile != null)
+                {
+                    var _headerValue = ContentDispositionHeaderValue.Parse(FormFile.ContentDisposition);
+                    _fieldName = _headerValue?.Name ?? string.Empty;
+                }
+
+                return _fieldName;
+            }
+            set
+            {
+                _fieldName = value;
             }
         }
 
@@ -266,16 +548,17 @@ namespace Td.AspNet.Upload
         /// <summary>
         /// 文件类型
         /// </summary>
-        public string ContentType
+        public override string ContentType
         {
             get
             {
-                if (string.IsNullOrWhiteSpace(_contentType))
+                if (string.IsNullOrWhiteSpace(_contentType) && FormFile != null)
                 {
+                    var _headerValue = ContentDispositionHeaderValue.Parse(FormFile.ContentDisposition);
                     //文件内容信息
-                    if (null != HeaderValue && null != HeaderValue.Parameters)
+                    if (null != _headerValue && null != _headerValue.Parameters)
                     {
-                        foreach (var p in HeaderValue.Parameters)
+                        foreach (var p in _headerValue.Parameters)
                         {
                             if (p.Name.Equals("ContentType", StringComparison.OrdinalIgnoreCase))
                             {
@@ -302,33 +585,91 @@ namespace Td.AspNet.Upload
         }
 
         /// <summary>
-        /// 是否为图片文件
+        /// 文件大小
         /// </summary>
-        public bool IsImage
+        public override long FileSize
         {
             get
             {
-                if (!string.IsNullOrWhiteSpace(ContentType))
-                {
-                    return ContentType.StartsWith("image/", StringComparison.OrdinalIgnoreCase);
-                }
-
-                return false;
-            }
-        }
-
-        private ContentDispositionHeaderValue _headerValue;
-        private ContentDispositionHeaderValue HeaderValue
-        {
-            get
-            {
-                if (_headerValue == null)
-                {
-                    _headerValue = ContentDispositionHeaderValue.Parse(FormFile.ContentDisposition);
-                }
-
-                return _headerValue;
+                return FormFile != null ? FormFile.Length : 0;
             }
         }
     }
+
+    /// <summary>
+    /// Base64FileContent
+    /// </summary>
+    public class Base64FileContent : FileContent
+    {
+        /// <summary>
+        /// 标准的Base64文件字符串表示,如：“data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAATgAAAE4==”
+        /// </summary>
+        public string Data { get; set; }
+
+        private string _fieldName;
+        /// <summary>
+        /// 文件标志名
+        /// </summary>
+        public override string FieldName
+        {
+            get
+            {
+                return _fieldName ?? string.Empty;
+            }
+            set { _fieldName = value; }
+        }
+
+        private string _contentType;
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        public override string ContentType
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_contentType) && !string.IsNullOrWhiteSpace(Data))
+                {
+                    Regex reg = new Regex(@"^data:(?<contenttype>[^;]+);base64,.+=+$", RegexOptions.IgnoreCase);
+
+                    Match m = reg.Match(Data);
+
+                    if (m.Success)
+                    {
+                        return m.Groups["contenttype"].Value;
+                    }
+                }
+
+                return _contentType;
+            }
+        }
+
+        private long _fileSize;
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public override long FileSize
+        {
+            get
+            {
+                if (_fileSize == 0)
+                {
+                    Regex reg = new Regex(@"^data:(?<contenttype>[^;]+);base64,(?<content>.+=+)$", RegexOptions.IgnoreCase);
+
+                    Match m = reg.Match(Data);
+
+                    if (m.Success)
+                    {
+                        string content = m.Groups["content"].Value;
+
+                        byte[] arr = Convert.FromBase64String(content);
+
+                        _fileSize = arr.Length;
+                    }
+                }
+                return _fileSize;
+            }
+        }
+    }
+
+    #endregion
 }
