364afae18011f7069595254873fcfa99217f3e10
伍朝辉 <337883612@qq.com>
2016-09-20 13:16:49 +0800

    新增自定义异常类

---
+++ Td.AspNet.Common/src/Td.AspNet.Utils/CustomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace Td.AspNet.Utils
+{
+    /// <summary>
+    /// 自定义异常
+    /// </summary>
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message) { }
+
+        public CustomException(string message, Exception inner) : base(message, inner) { }
+    }
+}
